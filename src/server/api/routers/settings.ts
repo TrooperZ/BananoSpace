efd2bedb365c79eea75545336c5f02d599b3e679
Trooper_Z <trooperz.tz@gmail.com>
2023-08-23 21:25:30 -0500

    Fix (final?)

--- src/server/api/routers/settings.ts
+++ src/server/api/routers/settings.ts
@@ -14,7 +14,6 @@ import { env } from "~/env.mjs";
 import BananoUtil from "@bananocoin/bananojs";
 import DepositUtil from "@bananocoin/bananojs";
 
-
 Main.setBananodeApiUrl("https://kaliumapi.appditto.com/api");
 
 export const settingsRouter = createTRPCRouter({
@@ -29,7 +28,8 @@ export const settingsRouter = createTRPCRouter({
 
       return updatedUser.address;
     }),
-    fetchAddress: protectedProcedure.input(z.object({id: z.string()}) )
+  fetchAddress: protectedProcedure
+    .input(z.object({ id: z.string() }))
     .query(async ({ ctx }) => {
       const userId = ctx.session.user.id;
       const user = await ctx.prisma.user.findUnique({
@@ -42,7 +42,8 @@ export const settingsRouter = createTRPCRouter({
 
       return user.address;
     }),
-    fetchBalance: protectedProcedure.input(z.object({id: z.string()}) )
+  fetchBalance: protectedProcedure
+    .input(z.object({ id: z.string() }))
     .query(async ({ ctx }) => {
       const userId = ctx.session.user.id;
       const user = await ctx.prisma.user.findUnique({
@@ -55,21 +56,26 @@ export const settingsRouter = createTRPCRouter({
 
       return user.balance;
     }),
-    processDeposit: protectedProcedure.input(z.string())
+  processDeposit: protectedProcedure
+    .input(z.string())
     .mutation(async ({ input: address, ctx }) => {
       const user = await ctx.prisma.user.findUnique({
         where: { id: ctx.session.user.id },
       });
-
-
+      if (!user) {
+        throw new Error("User not found");
+      }
 
       let total = 0.0;
       let targetHash = "";
       const mainAddr =
-      "ban_3boxjpo7symnd4pzoimc6wofa71sp6bb6n55y9axhypxkfuk7qh3aiukgte8";
+        "ban_3boxjpo7symnd4pzoimc6wofa71sp6bb6n55y9axhypxkfuk7qh3aiukgte8";
 
-    const pending = await BananodeApi.getAccountsPending([mainAddr], 25, "true").then( 
-      (accounts: any) => {
+      const pending = await BananodeApi.getAccountsPending(
+        [mainAddr],
+        25,
+        "true"
+      ).then((accounts: any) => {
         const pendingHashes =
           accounts.blocks
             .ban_3boxjpo7symnd4pzoimc6wofa71sp6bb6n55y9axhypxkfuk7qh3aiukgte8;
@@ -83,61 +89,70 @@ export const settingsRouter = createTRPCRouter({
             console.log("Found");
             console.log(total);
             break;
+          }
         }
-        }
+      });
 
-  
-      }
-      
-    );
-
-      const dep = await (DepositUtil as any).receiveBananoDepositsForSeed(env.BANANO_SEED, "0", "ban_1fomoz167m7o38gw4rzt7hz67oq6itejpt4yocrfywujbpatd711cjew8gjj", targetHash)
-      console.log(dep)
+      const dep = await (DepositUtil as any).receiveBananoDepositsForSeed(
+        env.BANANO_SEED,
+        "0",
+        "ban_1fomoz167m7o38gw4rzt7hz67oq6itejpt4yocrfywujbpatd711cjew8gjj",
+        targetHash
+      );
+      console.log(dep);
 
       const updatedBalance = await ctx.prisma.user.update({
         where: { id: ctx.session.user.id },
         data: { balance: user.balance + total },
       });
 
-
       return total;
     }),
-    processWithdraw: protectedProcedure.input(z.string())
+  processWithdraw: protectedProcedure
+    .input(z.string())
     .mutation(async ({ input: amount, ctx }) => {
-
       const user = await ctx.prisma.user.findUnique({
         where: { id: ctx.session.user.id },
       });
-      const balance = user.balance;
-      const newBalance = balance - parseFloat(amount);
-      const actAmount = parseFloat(amount) * 0.98;
-      const fee = parseFloat(amount) * 0.02
-      async function updateBalance () {
-        console.log(`deposit of ${user.address} successfully processed`)
-        
-        const updatedBalance = await ctx.prisma.user.update({
-          where: { id: ctx.session.user.id },
-          data: { balance: newBalance },
-        })
+      if (user != null && user != undefined) {
+        const actAmount = parseFloat(amount) * 0.98;
+        const fee = parseFloat(amount) * 0.02;
+        async function updateBalance() {
+          console.log(`deposit of ${user?.address} successfully processed`);
+          if (user!.balance != undefined){
+            const updatedBalance = await ctx.prisma.user.update({
+              where: { id: ctx.session.user.id },
+              data: { balance: user!.balance - actAmount },
+            });
+          }
+
+        }
+
+        async function fail() {
+          console.log(`deposit of ${user?.address} failed`);
+          
+        }
+        async function feeSend() {
+          console.log(`fee of ${user?.address} successfully processed`);
+        }
+        console.log();
+        const withdraw = await (BananoUtil as any).sendAmountToBananoAccount(
+          env.BANANO_SEED,
+          "0",
+          user.address,
+          actAmount * 10 ** 29,
+          updateBalance,
+          fail
+        );
+
+        const withdrawFee = await (BananoUtil as any).sendAmountToBananoAccount(
+          env.BANANO_SEED,
+          "0",
+          "ban_3zdr9i7o3knexoz7fjgae3r6u5osh7taq9e4wm5przniqjoh4idtrque55ag",
+          fee * 10 ** 29,
+          feeSend,
+          fail
+        );
       }
-
-      async function fail () {
-        console.log(`deposit of ${user.address} failed`)
-
-      }
-      async function feeSend () {
-        console.log(`fee of ${user.address} successfully processed`)
-
-      }
-      console.log()
-      const withdraw = await (BananoUtil as any).sendAmountToBananoAccount(env.BANANO_SEED, "0", user.address, (actAmount*(10**29)), updateBalance, fail)
-
-      const withdrawFee = await (BananoUtil as any).sendAmountToBananoAccount(env.BANANO_SEED, "0", "ban_3zdr9i7o3knexoz7fjgae3r6u5osh7taq9e4wm5przniqjoh4idtrque55ag", (fee*(10**29)), feeSend, fail)
-
-      
-      
     }),
-
-
 });
-
